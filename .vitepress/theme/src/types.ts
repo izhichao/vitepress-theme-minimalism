823b60d89132029b0eb482908d9f24ede5ca95f7
izhichao <43055550@qq.com>
2023-09-14 18:25:09 +0800

    chore: define the type file

---
+++ .vitepress/theme/src/types.ts
@@ -0,0 +1,22 @@
+import { DefaultTheme } from 'vitepress/theme';
+
+export interface IPost {
+  date: string;
+  title: string;
+  tags?: string[];
+  permalink?: string;
+  path: string;
+}
+
+export interface ITag {
+  [key: string]: IPost[];
+}
+
+export interface IArchive {
+  year: string;
+  data: IPost[];
+}
+
+export interface ThemeConfig extends DefaultTheme.Config {
+  posts: IPost[];
+}
