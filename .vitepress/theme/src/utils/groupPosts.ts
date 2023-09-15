365e61ab03a7d7a4da0bafe97198e4724432f9d3
izhichao <43055550@qq.com>
2023-09-15 13:05:48 +0800

    feat: create groupByYears function

---
+++ .vitepress/theme/src/utils/groupPosts.ts
@@ -0,0 +1,23 @@
+import { IPost, ITag, IArchive } from '../types';
+
+export const groupByYears = (posts: IPost[]) => {
+  const data: ITag = {};
+  posts.forEach((post) => {
+    const year = new Date(post.date).getFullYear();
+    if (year) {
+      if (!data[year]) {
+        data[year] = [];
+      }
+      data[year].push(post);
+    }
+  });
+
+  // get years and sort by year
+  const years = Object.keys(data).sort((a, b) => parseInt(b) - parseInt(a));
+
+  const sortedData: IArchive[] = [];
+  years.forEach((year) => {
+    sortedData.push({ year, data: data[year] });
+  });
+  return sortedData;
+};
