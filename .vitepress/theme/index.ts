b70b903d6b5bf6511d19fa17422bc5dd8b0ee964
izhichao <60996631+izhichao@users.noreply.github.com>
2023-09-16 15:54:56 +0800

    refactor: extract registerComponents funtion

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,16 +1,9 @@
 import DefaultTheme from 'vitepress/theme';
-import Home from './src/views/HomeView.vue';
-import Archives from './src/views/ArchivesView.vue';
-import Tags from './src/views/TagsView.vue';
-import Page from './src/views/PageView.vue';
-import './src/styles/index.less';
+import { registerComponents } from './src';
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
-    app.component('Home', Home);
-    app.component('Tags', Tags);
-    app.component('Archives', Archives);
-    app.component('Page', Page);
+    registerComponents(app);
   }
 };
