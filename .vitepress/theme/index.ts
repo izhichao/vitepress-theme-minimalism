4d76b179eed3d8e3ba73f623436024e6fc834d84
izhichao <60996631+izhichao@users.noreply.github.com>
2023-10-22 19:08:22 +0800

    feat: add image zoom function

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,25 @@
 import DefaultTheme from 'vitepress/theme';
+import mediumZoom from 'medium-zoom';
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
 import { registerComponents } from './src';
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     registerComponents(app);
+  },
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      mediumZoom('.main img', { background: 'rgba(0,0,0,0.2)' });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
   }
 };
