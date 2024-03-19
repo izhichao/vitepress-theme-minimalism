6ba2d68f58d0e1dc2fd8e7a659491b8b69265586
izhichao <60996631+izhichao@users.noreply.github.com>
2024-03-19 14:45:12 +0800

    feat: support comment

---
+++ .vitepress/theme/src/MinimalismLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme';
+import CommentItem from './components/CommentItem.vue';
+const { Layout } = DefaultTheme;
+</script>
+
+<template>
+  <Layout>
+    <template #doc-after>
+      <CommentItem />
+    </template>
+  </Layout>
+</template>
