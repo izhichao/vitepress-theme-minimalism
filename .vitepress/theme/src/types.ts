e4feba65295cc8e44aabe475e7c1098a106bd2de
izhichao <43055550@qq.com>
2024-03-19 19:54:25 +0800

    feat: comment config

--- .vitepress/theme/src/types.ts
+++ .vitepress/theme/src/types.ts
@@ -1,5 +1,14 @@
 import { DefaultTheme } from 'vitepress/theme';
-
+import {
+  WalineLocale,
+  WalineEmojiInfo,
+  WalineEmojiPresets,
+  WalineCommentSorting,
+  WalineImageUploader,
+  WalineHighlighter,
+  WalineTeXRenderer,
+  WalineSearchOptions
+} from '@waline/client';
 export interface IPost {
   title: string;
   datetime: string;
@@ -19,6 +28,23 @@ export interface IPage {
 
 export interface ICommnet {
   serverURL?: string;
+  lang?: string;
+  locale?: WalineLocale;
+  emoji?: (WalineEmojiInfo | WalineEmojiPresets)[] | false;
+  commentSorting?: WalineCommentSorting;
+  meta?: string[];
+  requiredMeta?: string[];
+  login?: string;
+  wordLimit?: number | [number, number];
+  pageSize?: number;
+  imageUploader?: WalineImageUploader | false;
+  highlighter?: WalineHighlighter | false;
+  texRenderer?: WalineTeXRenderer | false;
+  search?: WalineSearchOptions | false;
+  copyright?: boolean;
+  recaptchaV3Key?: string;
+  turnstileKey?: string;
+  reaction?: boolean | string[];
 }
 
 export interface ITag {
