b10a5d75fd4b780b065f50882c76799829de4840
izhichao <60996631+izhichao@users.noreply.github.com>
2024-06-16 17:13:24 +0800

    feat: support google adsense

---
+++ src/components/AdsenseItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <ins
+    class="adsbygoogle"
+    style="display: block"
+    :data-ad-client="client"
+    :data-ad-slot="slot"
+    data-ad-format="auto"
+    data-full-width-responsive="true"
+  ></ins>
+</template>
+
+<script lang="ts" setup>
+
+defineProps({
+  client: String,
+  slot: String
+});
+
+</script>
