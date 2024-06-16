03470becb5c6044d09052f1e46a18b5bcdfff905
izhichao <43055550@qq.com>
2024-06-17 00:14:36 +0800

    fix: fix AdsenseItem props bugs

--- src/components/AdsenseItem.vue
+++ src/components/AdsenseItem.vue
@@ -1,19 +1,28 @@
 <template>
-  <ins
-    class="adsbygoogle"
-    style="display: block"
-    :data-ad-client="client"
-    :data-ad-slot="slot"
-    data-ad-format="auto"
-    data-full-width-responsive="true"
-  ></ins>
+  <ClientOnly>
+    <ins
+      class="adsbygoogle"
+      style="display: block"
+      :data-ad-client="client"
+      :data-ad-slot="slot"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+  </ClientOnly>
 </template>
-
 <script lang="ts" setup>
-
+import { onMounted } from 'vue';
 defineProps({
   client: String,
   slot: String
 });
 
+onMounted(() => {
+  try {
+    // @ts-ignore
+    window.addAds();
+  } catch (e) {
+    console.log(e);
+  }
+});
 </script>
