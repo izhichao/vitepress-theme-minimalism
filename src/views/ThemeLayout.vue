f5a2c022036714e3b394f8719fee0aff8e101066
izhichao <60996631+izhichao@users.noreply.github.com>
2024-06-17 00:14:36 +0800

    fix: fix AdsenseItem props bugs

--- src/views/ThemeLayout.vue
+++ src/views/ThemeLayout.vue
@@ -1,52 +1,52 @@
 <template>
   <Layout>
     <template #page-top>
-      <AdsenseItem v-if="adsense && adsense['page-top']" :client="adsense.client" :slot="adsense['page-top'].slot" />
+      <AdsenseItem v-if="adsense && adsense['page-top']" :client="adsense.client" :slot="adsense['page-top']" />
     </template>
     <template #page-bottom>
       <AdsenseItem
         v-if="adsense && adsense['page-bottom']"
         :client="adsense.client"
-        :slot="adsense['page-bottom'].slot"
+        :slot="adsense['page-bottom']"
       />
     </template>
     <template #sidebar-nav-before>
       <AdsenseItem
         v-if="adsense && adsense['sidebar-nav-before']"
         :client="adsense.client"
-        :slot="adsense['sidebar-nav-before'].slot"
+        :slot="adsense['sidebar-nav-before']"
       />
     </template>
     <template #sidebar-nav-after>
       <AdsenseItem
         v-if="adsense && adsense['sidebar-nav-after']"
         :client="adsense.client"
-        :slot="adsense['sidebar-nav-after'].slot"
+        :slot="adsense['sidebar-nav-after']"
       />
     </template>
     <template #aside-outline-before>
       <AdsenseItem
         v-if="adsense && adsense['aside-outline-before']"
         :client="adsense.client"
-        :slot="adsense['aside-outline-before'].slot"
+        :slot="adsense['aside-outline-before']"
       />
     </template>
     <template #aside-outline-after>
       <AdsenseItem
         v-if="adsense && adsense['aside-outline-after']"
         :client="adsense.client"
-        :slot="adsense['aside-outline-after'].slot"
+        :slot="adsense['aside-outline-after']"
       />
     </template>
     <template #doc-before>
       <AdsenseItem
         v-if="adsense && adsense['doc-before']"
         :client="adsense.client"
-        :slot="adsense['doc-before'].slot"
+        :slot="adsense['doc-before']"
       />
     </template>
     <template #doc-after>
-      <AdsenseItem v-if="adsense && adsense['doc-after']" :client="adsense.client" :slot="adsense['doc-after'].slot" />
+      <AdsenseItem v-if="adsense && adsense['doc-after']" :client="adsense.client" :slot="adsense['doc-after']" />
       <CommentItem />
     </template>
     <template #doc-footer-before><PostInfoItem /></template>
