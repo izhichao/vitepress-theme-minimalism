b725f5bcf06176c5fbc5c1ff16d0d866f3601693
izhichao <43055550@qq.com>
2024-06-20 21:31:56 +0800

    feat: support custom ads

--- src/types.ts
+++ src/types.ts
@@ -45,16 +45,33 @@ export interface ICommnet {
   reaction?: boolean | string[];
 }
 
+export interface IAd {
+  title: string;
+  img: string;
+  link?: string;
+}
+
+export interface IAds {
+  'page-top'?: (IAd | IAd[])[];
+  'page-bottom'?: (IAd | IAd[])[];
+  'sidebar-nav-before'?: (IAd | IAd[])[];
+  'sidebar-nav-after'?: (IAd | IAd[])[];
+  'aside-outline-before'?: (IAd | IAd[])[];
+  'aside-outline-after'?: (IAd | IAd[])[];
+  'doc-before'?: (IAd | IAd[])[];
+  'doc-after'?: (IAd | IAd[])[];
+}
+
 export interface IAdsense {
-  client?: String;
-  'page-top'?: String;
-  'page-bottom'?: String;
-  'sidebar-nav-before'?: String;
-  'sidebar-nav-after'?: String;
-  'aside-outline-before'?: String;
-  'aside-outline-after'?: String;
-  'doc-before'?: String;
-  'doc-after'?: String;
+  client?: string;
+  'page-top'?: string;
+  'page-bottom'?: string;
+  'sidebar-nav-before'?: string;
+  'sidebar-nav-after'?: string;
+  'aside-outline-before'?: string;
+  'aside-outline-after'?: string;
+  'doc-before'?: string;
+  'doc-after'?: string;
 }
 
 export interface ITag {
@@ -70,5 +87,6 @@ export interface ThemeConfig extends DefaultTheme.Config {
   posts: IPost[];
   page?: IPage;
   comment?: ICommnet;
+  ads?: IAds;
   adsense?: IAdsense;
 }
