e605056e7d75b0b054eabf89a8f967881c7c735e
izhichao <60996631+izhichao@users.noreply.github.com>
2024-06-22 22:45:36 +0800

    feat: auto generate category.md and tags.md

---
+++ src/utils/generateMd.ts
@@ -0,0 +1,16 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileExists } from './fileExists';
+
+export const generateMd = async (type: string) => {
+  const filePath = path.resolve(`${type.toLowerCase()}.md`);
+  if (await fileExists(filePath)) return;
+
+  const page = `
+---
+title: ${type}
+layout: ${type}
+---
+    `.trim();
+  await fs.writeFile(path.resolve(filePath), page);
+};
