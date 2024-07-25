270089fd0f2a8161a6bf37b97997e5870d7682f6
izhichao <60996631+izhichao@users.noreply.github.com>
2024-07-25 16:50:18 +0800

    chore: update demo config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -29,24 +29,34 @@ export default defineConfigWithTheme<ThemeConfig>({
     ads: {
       asideOutlineAfter: [
         {
-          title: 'ouo.io - 縮短網址也可以賺錢',
-          img: 'https://ouo.io/images/banners/r5.jpg',
-          link: 'http://ouo.io/ref/QQbUaFAo'
+          title: '关注微信公众号',
+          img: 'https://minio.zhichao.org/assets/wechat.png'
         }
       ],
       sidebarNavAfter: [
         {
-          title: 'ouo.io - 縮短網址也可以賺錢',
-          img: 'https://ouo.io/images/banners/r5.jpg',
-          link: 'http://ouo.io/ref/QQbUaFAo'
+          title: '关注微信公众号',
+          img: 'https://minio.zhichao.org/assets/wechat.png'
         }
       ],
       docAfter: [
-        {
-          title: 'ouo.io - 縮短網址也可以賺錢',
-          img: 'https://ouo.io/images/banners/r1.jpg',
-          link: 'http://ouo.io/ref/QQbUaFAo'
-        }
+        [
+          {
+            title: 'V.PS- 美国 CN2 GIA / 9929 / CMIN2 顶级线路',
+            img: 'https://minio.zhichao.org/assets/vps.png',
+            link: 'https://vps.hosting/?cmd=cart&action=add&affid=816&id=144'
+          },
+          {
+            title: '搬瓦工 - 美国 CN2 优化线路',
+            img: 'https://minio.zhichao.org/assets/bwh.png',
+            link: 'https://bwh81.net/aff.php?aff=46644&pid=44'
+          },
+          {
+            title: 'RackNerd - 美国 163 直连线路',
+            img: 'https://minio.zhichao.org/assets/rn.png',
+            link: 'https://my.racknerd.com/aff.php?aff=6884&pid=810'
+          }
+        ]
       ]
     },
     logo: '/profile.png',
@@ -75,12 +85,9 @@ export default defineConfigWithTheme<ThemeConfig>({
     },
     socialLinks: [{ icon: 'github', link: 'https://github.com/izhichao/vitepress-theme-minimalism' }],
     footer: {
-      message: `友情链接：<a href="https://bwh81.net/aff.php?aff=46644" target="_blank">搬瓦工</a>&nbsp;
-      <a href="https://vps.hosting/?affid=816" target="_blank">V.PS</a>&nbsp;
-      <a href="https://my.racknerd.com/aff.php?aff=6884" target="_blank">RackNerd</a>
-      `,
-      copyright: `Theme by <a href="https://github.com/izhichao/vitepress-theme-minimalism" target="_blank">Minimalism</a><br />
-        Copyright © 2017-2024 <a href="https://github.com/izhichao" target="_blank">只抄</a>`
+      message:
+        'Theme by <a href="https://github.com/izhichao/vitepress-theme-minimalism" target="_blank">Minimalism</a>',
+      copyright: 'Copyright © 2017-2024 <a href="https://github.com/izhichao" target="_blank">只抄</a>'
     },
     search: { provider: 'local' }
   },
