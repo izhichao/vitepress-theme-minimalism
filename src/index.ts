90473e50f6b1c26ac0634a35b9f1ee7ff8b80488
izhichao <60996631+izhichao@users.noreply.github.com>
2025-12-09 20:30:53 +0800

    feat: page transition progress bar

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import Group from './views/GroupView.vue';
 import Page from './views/PageView.vue';
 import Password from './views/PasswordView.vue';
 import { bindFancybox, destroyFancybox } from './utils/fancybox';
+import { BProgress } from '@bprogress/core';
+import '@bprogress/core/css';
 import './styles/index.less';
 
 export default {
@@ -16,10 +18,13 @@ export default {
     app.component('Page', Page);
     app.component('Password', Password);
     if (inBrowser) {
+      BProgress.configure({ showSpinner: false });
       router.onBeforeRouteChange = () => {
+        BProgress.start();
         destroyFancybox(); // 销毁图片查看器
       };
       router.onAfterRouteChange = () => {
+        BProgress.done();
         bindFancybox(); // 绑定图片查看器
       };
     }
