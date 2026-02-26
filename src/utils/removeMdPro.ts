26cfdae27f4c0d1704d7ef17ffa398608f0b94e8
izhichao <43055550@qq.com>
2026-02-26 11:22:32 +0800

    feat: remove quote when generating excerpts

--- src/utils/removeMdPro.ts
+++ src/utils/removeMdPro.ts
@@ -1,7 +1,12 @@
 import removeMd from 'remove-markdown';
 
 export const removeMdPro = (str: string) => {
-  return removeMd(str.replace(/```.*?```/gs, '').replace(/^#+\s.*$/gm, '')) // 移除代码块、标题
+  return removeMd(
+    str
+      .replace(/```.*?```/gs, '')
+      .replace(/^#+\s.*$/gm, '')
+      .replace(/^>.*$/gm, '')
+  ) // 移除代码块、标题、引用
     .trim() // 移除空格
     .split(/\r\n|\n|\r/)
     .join(' ') // 移除换行
