c8d4977ba1c3cda189ee9ff058c6d1cd9a83a936
izhichao <60996631+izhichao@users.noreply.github.com>
2026-03-03 12:12:14 +0800

    chore: update demo config

--- .vitepress/theme/ads.ts
+++ .vitepress/theme/ads.ts
@@ -2,6 +2,11 @@ import type { IAd } from '../../src/types.ts';
 
 export const ads: (IAd | IAd[])[] = [
   {
+    title: 'LocalHost',
+    img: 'https://img.erpweb.eu.org/imgs/2026/03/fac80ea0fd52f30f.gif',
+    link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+  },
+  {
     title: 'RackNerd',
     img: 'https://racknerd.com/banners/728x90.gif',
     link: 'https://my.racknerd.com/aff.php?aff=6884'
@@ -10,5 +15,5 @@ export const ads: (IAd | IAd[])[] = [
     title: 'Evoxt',
     img: 'https://console.evoxt.com/affiliates-assets/english/728x90.png',
     link: 'https://console.evoxt.com/aff.php?aff=2377'
-  },
+  }
 ];
